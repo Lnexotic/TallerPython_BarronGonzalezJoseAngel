8e0da0f3328ef503a827c303f1b87e9060c43fba
Barrón González José Ángel <36345418+Lnexotic@users.noreply.github.com>
2025-11-30 08:13:15 -0600

    Add files via upload

---
+++ TallerPython_BarronGonzalezJoseAngel/scr/example.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_excel('datos.xlsx')
+df.head()
+
+df['prom'] = df[['Parcial 1', 'Parcial 2', 'Parcial 3']].mean(axis=1)
+df['estatus'] = np.where(df['prom'] >= 7, 'Aprobado', 'Reprobado')
+
+df.to_excel('datos_mod.xlsx', index=False)
